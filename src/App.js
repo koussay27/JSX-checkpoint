35001dad67f44b4193c1353d57ffc3bae3faf3ca
koussay27 <koussaymoujahed27@gmail.com>
2021-04-19 13:16:08 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
+import imageInSrc from "./imageInSrc.jpg"
+import ReactPlayer from "react-player"
+
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div style={{border:'solid 1px black',maxWwidth:'100vw'}}>
+
+ <h1 className="title-red">Koussay</h1>
+
+ 
+
+ <img src={imageInSrc} alt ='imageInSrc' ></img>
+
+ 
+
+ <img src="/imageInPublic.jpg" alt="myimage" ></img>
+</div>
+
+<div>
+      <ReactPlayer
+        url="https://www.youtube.com/watch?v=fibYknUCIU4"
+      />
+    </div>
       </header>
     </div>
   );
